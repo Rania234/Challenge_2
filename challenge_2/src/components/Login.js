d1bcf9132dbb625d0d8f0fbc7cde9dca3cc9ec7b
nour <boumenjel51@gmail.com>
2021-12-06 09:45:25 +0100

    loginPage

---
+++ challenge_2/src/components/Login.js
@@ -0,0 +1,18 @@
+const Login = () => {
+    return (
+        <div  >
+            <div>
+        <span>Login page</span>
+        </div>
+        <form >
+        <input type="email" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+       
+        <button>Valider</button>
+        </form>
+      </div>
+    )
+
+
+}
+export default Login ;
