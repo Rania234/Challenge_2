76f8af9229baf73e750a83e8e2d7cf2bd79b8190
Rania <raniaboularess@gmail.com>
2021-12-06 09:41:15 +0100

    page home

---
+++ challenge_2/src/Components/hello/Hello.js
@@ -0,0 +1,14 @@
+
+import Menu from "../menu/Menu"
+
+import "./Hello.css"
+
+function Hello() {
+  return (
+    <div className="hello">
+     <Menu />
+      <div></div>
+    </div>
+  )
+}
+export default Hello
